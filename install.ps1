599a158b804359d2b9902dd3ea13d37912ec9f51
nguyendangkin <nguyenchin0077@gmail.com>
2025-08-16 13:03:51 +0700

    fix utf8

--- install.ps1
+++ install.ps1
@@ -2,17 +2,14 @@ $exeUrl = "https://github.com/nguyendangkin/chin-pak/releases/download/v1.0.0/ch
 $installDir = "$env:ProgramFiles\Chin"
 $exePath = Join-Path $installDir "chin.exe"
 
-# Tạo folder cài đặt
 if (!(Test-Path $installDir)) {
     New-Item -ItemType Directory -Path $installDir | Out-Null
 }
 
-# Tải exe từ GitHub release
 Write-Host "Dang tai chin.exe..."
 Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
-# Thêm vào PATH nếu chưa có
-$target = "Machine"  # hoặc "User" nếu chỉ muốn cho user hiện tại
+$target = "Machine"
 $oldPath = [Environment]::GetEnvironmentVariable("Path", $target)
 if ($oldPath -notlike "*$installDir*") {
     [Environment]::SetEnvironmentVariable("Path", "$oldPath;$installDir", $target)
